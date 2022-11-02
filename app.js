10df8a77aa92d14f8474f082cb0a9369834afd3d
donthireddylokeshreddy <111790773+donthireddylokeshreddy@users.noreply.github.com>
2022-11-02 11:12:32 -0500

    added images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var grocaryRouter = require('./routes/grocary');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grocary', grocaryRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
